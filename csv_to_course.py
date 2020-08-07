98bfa782a5786d509a01b7059924f19a72222795
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2020-08-07 15:10:04 -0300

    Add csv_to_course

---
+++ csv_to_course.py
@@ -0,0 +1,45 @@
+import os, sys
+import csv
+import json
+
+if getattr(sys, 'frozen', False):
+    # If we're running as a pyinstaller bundle
+    SCRIPT_DIR = os.path.dirname(sys.executable)
+else:
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+csv_file = os.path.join(SCRIPT_DIR, 'CoordRecorder_CSV.txt')
+json_file = os.path.join(SCRIPT_DIR, 'CoordRecorder_Course.json')
+
+def point(c):
+    return { 'X': float(c[0]), 'Y': float(c[1]), 'Z': float(c[2]) }
+
+if os.path.isfile(csv_file):
+    with open(csv_file, 'r') as fd:
+        coords = [tuple(line) for line in csv.reader(fd)]
+else:
+    print(f'File {csv_file} not found.')
+    if getattr(sys, 'frozen', False):
+        input()
+    sys.exit()
+
+data = {}
+data['Name'] = "CoordRecorder_Course"
+data['StartPoint'] = point(coords[0])
+data['Props'] = []
+start = { 'Name': 'prop_tri_start_banner', 'Position': point(coords[1]), 'Rotation': point([0, 0, 0]) }
+data['Props'].append(start)
+finish = { 'Name': 'prop_tri_finish_banner', 'Position': point(coords[-1]), 'Rotation': point([0, 0, 0]) }
+data['Props'].append(finish)
+del coords[0:2]
+del coords[-1]
+data['WayPointList'] = []
+for c in coords:
+    data['WayPointList'].append(point(c))
+
+with open(json_file, 'w', encoding='utf-8') as fd:
+    json.dump(data, fd, indent=2)
+
+print(f'File {json_file} saved.')
+if getattr(sys, 'frozen', False):
+    input()
